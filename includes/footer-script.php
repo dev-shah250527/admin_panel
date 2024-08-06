3801db65f862cf3210f7c85165d77ad2514def8a
Dev <devrshah2505@gmail.com>
2024-08-06 12:57:34 +0530

    added includes files

---
+++ includes/footer-script.php
@@ -0,0 +1,16 @@
+<!-- App js -->
+<script src="assets/js/vendor.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+<!-- Knob charts js -->
+<script src="assets/libs/jquery-knob/jquery.knob.min.js"></script>
+
+<!-- Sparkline Js-->
+<script src="assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+<script src="assets/libs/morris.js/morris.min.js"></script>
+
+<script src="assets/libs/raphael/raphael.min.js"></script>
+
+<!-- Dashboard init-->
+<script src="assets/js/pages/dashboard.js"></script>
